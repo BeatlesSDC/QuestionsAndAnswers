578067046abea43b9feb0c762d634be5acede16f
Phil Teves <phillip.teves@gmail.com>
2021-03-21 20:00:44 -0400

    implemented db queries for all server routes

--- db/index.js
+++ db/index.js
@@ -10,31 +10,129 @@ connection.connect()
   .then(()=>{console.log('Postgres connection successful...');})
   .catch((err)=>{console.log('Connection error: ', err);})
 
-const getQandA = (cb) => {
+const getQandA = (product, cb) => {
   //query for questions, query for answers
+  const quesQuery = `SELECT * FROM questions WHERE product_id = ${product}`;
+  connection.query(quesQuery, (err, res) => {
+    if (err) {
+      cb(err, null);
+    } else {
+      let questions = res;
+      //FIX THE BELOW
+      const ansQuery = 'SELECT * FROM answers'; //how do i filter for question IDs?? -> JOINS????
+      connection.query(ansQuery, (error, result) => {
+        if (error) {
+          cb(error, null);
+        } else {
+          let answers = result;
+          cb(null, {questions: questions, answers: answers});
+        }
+      });
+    }
+  })
 };
 
 const postQuestion = (question, cb) => {
-
+  // question === {name, body, email, product_id}
+  const query = `INSERT INTO questions (product_id, question_body, asker_name, question_helpfulness)
+  VALUES (${question.product_id}, ${question.body}, ${question.name}, 0)`;
+  connection.query(query, (err, res) => {
+    if (err) {
+      cb(err);
+    } else {
+      cb(null);
+    }
+  });
 };
 
-const postAnswer = (answer, cb) => {
-
+const postAnswer = (answer, question, cb) => {
+  // answer === {name, body, email}
+  const query = `INSERT INTO answers (question_id, answer_body, answerer_name, helpfulness)
+  VALUES (${question}, ${answer.body}, ${answer.name}, 0)`;
+  connection.query(query, (err, res) => {
+    if (err) {
+      cb(err);
+    } else {
+      cb(null);
+    }
+  });
 };
 
 const upvoteQuestion = (id, cb) => {
-
+  const getHelp = `SELECT question_helpfulness FROM questions WHERE question_id = ${id}`;
+  connection.query(getHelp, (error, data) => {
+    if (error) {
+      console.error(`Error fetching helpfulness value for question ${id}. Error: `, error);
+      cb(err);
+    } else {
+      console.log('Current helpfulness: ', data);
+      data++;
+      const setHelp = `UPDATE questions SET question_helpfulness = ${data} WHERE question_id = ${id}`;
+      connection.query(setHelp, (err, res) => {
+        if (err) {
+          cb(err);
+        } else {
+          cb(null);
+        }
+      })
+    }
+  })
 }
 
 const upvoteAnswer = (id, cb) => {
-
+  const getHelp = `SELECT answer_helpfulness FROM answers WHERE answer_id = ${id}`;
+  connection.query(getHelp, (error, data) => {
+    if (error) {
+      console.error(`Error fetching helpfulness value for answer ${id}. Error: `, error);
+      cb(err);
+    } else {
+      console.log('Current helpfulness: ', data);
+      data++;
+      const setHelp = `UPDATE answers SET answer_helpfulness = ${data} WHERE answer_id = ${id}`;
+      connection.query(setHelp, (err, res) => {
+        if (err) {
+          cb(err);
+        } else {
+          cb(null);
+        }
+      })
+    }
+  })
 }
 
 const reportAnswer = (id, cb) => {
-
+  const getAnswer = `SELECT * FROM answers WHERE answer_id = ${id}`;
+  connection.query(getAnswer, (error, data) => {
+    if (err) {
+      cb(error);
+    } else {
+      console.log('fetched answer ', data);
+      //insert into 'reportedanswers'
+      const addReported = ``; //LOOK AT DATA && WRITE ME!!!
+      connection.query(addReported, (err, res) => {
+        if (err) {
+          cb(err);
+        } else {
+          const removeAnswer = `DELETE FROM answers WHERE answer_id = ${id}`;
+          connection.query(removeAnswer, (failure, success) => {
+            if (failure) {
+              cb(failure);
+            } else {
+              cb(null);
+            }
+          });
+        }
+      });
+      //delete from 'answers'
+    }
+  });
 }
+
 module.exports = {
   getQandA,
   postQuestion,
-  postAnswer
+  postAnswer,
+  upvoteQuestion,
+  upvoteAnswer,
+  reportAnswer
 }
--- server/app.js
+++ server/app.js
@@ -13,13 +13,18 @@ app.use(express.urlencoded({extended:true}));
 
 //routes
 app.get('/qa/questions', (req, res) => {
-  db.getQandA((err, data) => {
+  db.getQandA(req.params.product_id, (err, data) => {
     if (err) {
-      console.error('Error retrieving question & answer data');
+      console.error('Error retrieving question & answer data. Error: ', err);
       res.sendStatus(404);
     } else {
       console.log('Successfully fetched question & answer data');
-      //format data as client expects it (answers go in questions)
+      //data === {questions: query-results, answers: query-results}
+      //iterate thru & format data as client expects it (answers go in questions, reported = false)
+        //iterate data.answers, create a 'qIDs' obj w question_id keys & push answers to array
+          //also give each answer a 'reported: false' attr
+        //iterate data.questions & create an 'answers' attr in each, assigning it a value of qIDs[question_id]
+          //also give each question a 'reported: false' attr
     }
   });
 });
@@ -27,7 +32,7 @@ app.get('/qa/questions', (req, res) => {
 app.post('/qa/questions', (req, res) => {
   db.postQuestion(req.body, (err, result) => {
     if (err) {
-      console.error('Failed to write question to database');
+      console.error('Failed to write question to database. Error: ', err);
     } else {
       console.log('Successfully posted question');
       res.sendStatus(200);
@@ -36,9 +41,9 @@ app.post('/qa/questions', (req, res) => {
 });
 
 app.post('/qa/questions/:question_id/answers', (req, res) => {
-  db.postAnswer(req.body, (err, result) => {
+  db.postAnswer(req.body, req.params.question_id, (err) => {
     if (err) {
-      console.log('Failed to write answer to database');
+      console.log('Failed to write answer to database. Error: ', err);
       res.sendStatus(400);
     } else {
       console.log('Successfully posted answer');
@@ -48,9 +53,9 @@ app.post('/qa/questions/:question_id/answers', (req, res) => {
 });
 
 app.put('/qa/questions/:question_id/helpful', (req, res) => {
-  db.upvoteQuestion(req.params.question_id, (err, result) => {
+  db.upvoteQuestion(req.params.question_id, (err) => {
     if (err) {
-      console.error(`Failed to update question #${req.params.question_id}'s helpfulness`);
+      console.error(`Failed to update question #${req.params.question_id}'s helpfulness. Error: `, err);
       res.sendStatus(400);
     } else {
       console.log('Successfully updated question helpfulness');
@@ -62,7 +67,7 @@ app.put('/qa/questions/:question_id/helpful', (req, res) => {
 app.put('/qa/answers/:answer_id/helpful', (req, res) => {
   db.upvoteAnswer(req.params.answer_id, (err, result) => {
     if (err) {
-      console.error(`Failed to update answer #${req.params.answer_id}'s helpfulness`)
+      console.error(`Failed to update answer #${req.params.answer_id}'s helpfulness. Error: `, err);
       res.sendStatus(400);
     } else {
       console.log('Successfully updated answer helpfulness');
@@ -74,7 +79,7 @@ app.put('/qa/answers/:answer_id/helpful', (req, res) => {
 app.put('/qa/answers/:answer_id/report', (req, res) => {
   db.reportAnswer(req.params.answer_id, (err, result) => {
     if (err) {
-      console.error(`Failed to report answer #${req.params.answer_id}`);
+      console.error(`Failed to report answer #${req.params.answer_id}. Error: `, err);
       res.sendStatus(400);
     } else {
       console.log('Successfully reported answer');
